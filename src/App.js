5ce07b9a8e22b65f456224fbccf366f0b2bf23d6
Jakub Jereczek <99jakubjereczek@gmail.com>
2021-01-17 19:08:02 +0100

    style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className="app">
+      <div class="row">
+        <div class="numbers">
+          <h2>Uzupełnij liczby, które potrzebne są do obliczenia działania</h2>
+          <input type="number"></input>
+          <input type="number"></input>
+        </div>
+        <div class="operators">
+          <div class="box active">
+            <p>Dodawanie</p>
+          </div>
+          <div class="box">
+            <p>Odejmowanie</p>
+          </div>
+          <div class="box">
+            <p>Mnożenie</p>
+          </div>
+          <div class="box">
+            <p>Dzielenie</p>
+          </div>
+          <div class="box">
+            <p>Pierwiastek</p>
+          </div>
+          <div class="box">
+            <p>Wartość bezwzględna</p>
+          </div>
+          <div class="box">
+            <p>Logarytm naturalny</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="result">
+          <h2>Wynik działania jest równy</h2>
+          <h3>0</h3>
+        </div>
+      </div>
     </div>
   );
 }
